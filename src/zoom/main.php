b97c281ce7ae30b771e8f0c8aea060f5fdb7e801
Mattemmodder <mathewpsutherland@gmail.com>
2018-01-27 13:43:34 +0000

    Update main.php

--- src/zoom/main.php
+++ src/zoom/main.php
@@ -30,8 +30,6 @@ class Main extends PluginBase implements Listener {
 		$effect = Effect::getEffect($this->config->get("effect"));
 		$player = $event->getPlayer();
 		$effect->setDuration($this->config->get("duration"));
-		$effect->Effect($this->config->get("effect"));
-		$effect->Effect($this->config->get("duration"));
 		$player = getServer()->getPlayer($args[0]);
 		$player->addEffect($effect);
 		
