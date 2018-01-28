1f4056b59671e3a6bc2617b728f5489088dafb68
Mattemmodder <mathewpsutherland@gmail.com>
2018-01-28 16:50:55 +0000

    Code change
    Added events
    replaced death with chat
    need to change config

--- src/zoom/main.php
+++ src/zoom/main.php
@@ -1,6 +1,5 @@
 <?php
 namespace zoom;
-
 use pocketmine\plugin\PluginBase;
 use pocketmine\Player;
 use pocketmine\entity\Effect;
@@ -10,7 +9,8 @@ use pocketmine\command\utils\InvalidCommandSyntaxException;
 use pocketmine\event\TranslationContainer;
 use pocketmine\utils\TextFormat;
 use pocketmine\event\player\PlayerJoinEvent;
-use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\event\player\PlayerChangeSkinEvent;
 use pocketmine\CommandReader;
 use pocketmine\CommandExecuter;
 use pocketmine\command\defaults\EffectCommand;
@@ -23,27 +23,25 @@ class Main extends PluginBase implements Listener {
 	
 	public function onEnable() {
 		@mkdir($this->getDataFolder());
-		$this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML, array("effect" => 18, "duration" => 18, "DeathEffect" => 18, "DeathDuration" => 18));
+		$this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML, array("effect" => 18, "duration" => 18, "MessageEffect" => 18, "MessageDuration" => 18));
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 	}
 	
 	public function onJoin(PlayerJoinEvent $event) {
 		$effect = Effect::getEffect($this->config->get("effect"));
-		$player = $event->getPlayer();
 		$effect->setDuration($this->config->get("duration") * 18);
+		$player = $event->getPlayer();
 		$player->addEffect($effect);
 		var_dump($effect);
 		
 	}
 	
-	public function onDeath(PlayerDeathEvent $event) {
-		$effect = Effect::getEffect($this->config->get("DeathEffect"));
+	public function onChat(PlayerChatEvent $event) {
+		$effect = Effect::getEffect($this->config->get("MessageEffect"));
 		$player = $event->getPlayer();
-		$killer = $player->getDamager();
-		$effect->setDuration($this->config->get("DeathDuration") * 18);
-		$killer->addEffect($effect);
+		$effect->setDuration($this->config->get("MessageDuration") * 18);
+		$player->addEffect($effect);
 		var_dump($effect);
 		
 	}
-
 }
