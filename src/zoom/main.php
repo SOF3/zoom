08b19cbb4024694b0808eab8383a78aac9e6eac9
Mattemmodder <mathewpsutherland@gmail.com>
2018-01-31 08:25:16 +0000

    Update main.php

--- src/zoom/main.php
+++ src/zoom/main.php
@@ -10,20 +10,29 @@ use pocketmine\event\TranslationContainer;
 use pocketmine\utils\TextFormat;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerChatEvent;
-use pocketmine\event\player\PlayerChangeSkinEvent;
+use pocketmine\event\entity\EntityArmorChangeEvent;
+use pocketmine\event\player\PlayerEditBookEvent;
+use pocketmine\event\player\PlayerGameModeChangeEvent;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\player\PlayerBedLeaveEvent;
+use pocketmine\event\entity\EntityShootBowEvent;
+use pocketmine\event\entity\EntitySpawnEvent;
+use pocketmine\event\entity\EntityTeleportEvent;
+use pocketmine\event\entity\EntityInventoryChangeEvent;
+use pocketmine\event\entity\EntityBlockChangeEvent;
+use pocketmine\event\entity\EntityMotionEvent;
 use pocketmine\CommandReader;
 use pocketmine\CommandExecuter;
 use pocketmine\command\defaults\EffectCommand;
-
 # CODE BY Cat 'Discord'
-
-class Main extends PluginBase implements Listener {
+class Main extends PluginBase implements listener {
 	
 	private $config;
 	
 	public function onEnable() {
+		$this->getLogger()->info("Created by Cat -Discord- ");
 		@mkdir($this->getDataFolder());
-		$this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML, array("effect" => 18, "duration" => 18, "MessageEffect" => 18, "MessageDuration" => 18));
+		$this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML, array("effect" => 18, "duration" => 18, "MessageEffect" => 18, "MessageDuration" => 18, "ArmorEffect" => 18, "ArmorDuration" => 18, "EditBookEffect" => 18, "EditBookDuration" => 18, "GamemodeChangeEffect" => 18, "GamemodeChangeDuration" => 18, "InteractEffect" => 18, "InteractDuration" => 18, "BowEffect" => 18, "BowDuration" => 18, "TeleportEffect" => 18, "TeleportDuration" => 18, "LeaveBedEffect" => 18, "LeaveBedDuration" => 18, "InventoryChangeEffect" => 18, "InventoryChangeDuration" => 18, "BlockChangeEffect" => 18, "BlockChangeDuration" => 18));
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 	}
 	
@@ -32,7 +41,6 @@ class Main extends PluginBase implements Listener {
 		$effect->setDuration($this->config->get("duration") * 18);
 		$player = $event->getPlayer();
 		$player->addEffect($effect);
-		var_dump($effect);
 		
 	}
 	
@@ -41,7 +49,78 @@ class Main extends PluginBase implements Listener {
 		$player = $event->getPlayer();
 		$effect->setDuration($this->config->get("MessageDuration") * 18);
 		$player->addEffect($effect);
-		var_dump($effect);
 		
 	}
+				     
+	public function onArmorChange(EntityArmorChangeEvent $event) {
+		$effect = Effect::getEffect($this->config->get("ArmorEffect"));
+		$entity = $event->getEntity();
+		$effect->setDuration($this->config->get("ArmorDuration") * 18);
+		$entity->addEffect($effect);
+					
+		}	
+	
+	public function onEditBook(PlayerEditBookEvent $event) {
+		$effect = Effect::getEffect($this->config->get("EditBookEffect"));
+		$player = $event->getPlayer();
+		$effect->setDuration($this->config->get("EditBookDuration") * 18);
+		$player->addEffect($effect);
+					
+		}
+	
+	public function onGamemodeChange(PlayerGameModeChangeEvent $event) {
+		$effect = Effect::getEffect($this->config->get("GamemodeChangeEffect"));
+		$player = $event->getPlayer();
+		$effect->setDuration($this->config->get("GamemodeChangeDuration") * 18);
+		$player->addEffect($effect);
+					
+		}
+	
+	public function onInteract(PlayerInteractEvent $event) {
+		$effect = Effect::getEffect($this->config->get("InteractEffect"));
+		$player = $event->getPlayer();
+		$effect->setDuration($this->config->get("InteractDuration") * 18);
+		$player->addEffect($effect);
+					
+		}
+	
+	public function onShootBow(EntityShootBowEvent $event) {
+		$effect = Effect::getEffect($this->config->get("BowEffect"));
+		$entity = $event->getEntity();
+		$effect->setDuration($this->config->get("BowDuration") * 18);
+		$entity->addEffect($effect);
+					
+		}
+	
+	public function onTeleport(EntityTeleportEvent $event) {
+		$effect = Effect::getEffect($this->config->get("TeleportEffect"));
+		$entity = $event->getEntity();
+		$effect->setDuration($this->config->get("TeleportDuration") * 18);
+		$entity->addEffect($effect);
+					
+		}
+	
+	public function onLeaveBed(PlayerBedLeaveEvent $event) {
+		$effect = Effect::getEffect($this->config->get("LeaveBedEffect"));
+		$player = $event->getPlayer();
+		$effect->setDuration($this->config->get("LeaveBedDuration") * 18);
+		$player->addEffect($effect);
+					
+		}
+	
+	public function onInventoryChange(EntityInventoryChangeEvent $event) {
+		$effect = Effect::getEffect($this->config->get("InventoryChangeEffect"));
+		$entity = $event->getEntity();
+		$effect->setDuration($this->config->get("InventoryChangeDuration") * 18);
+		$entity->addEffect($effect);
+					
+		}
+	
+	public function onBlockChange(EntityBlockChangeEvent $event) {
+		$effect = Effect::getEffect($this->config->get("BlockChangeEffect"));
+		$entity = $event->getEntity();
+		$effect->setDuration($this->config->get("BlockChangeDuration") * 18);
+		$entity->addEffect($effect);
+					
+		}
 }
